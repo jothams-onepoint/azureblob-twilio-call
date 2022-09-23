d11b03f38fe1c53a34b079ae24aa825737f8dbef
JothamStickings <jotham.stickings@onepointltd.com>
2022-09-23 15:24:49 +0100

    add console logs

--- fn-blob-to-twilio-call/twilioClient.ts
+++ fn-blob-to-twilio-call/twilioClient.ts
@@ -11,7 +11,10 @@ const client = new Twilio(accountSid, authToken);
 export async function createTwilioCalls(url: string): Promise<{operator: string, response: CallInstance}[]> {
     // make calls using twilio
     const results: {operator: string, response: CallInstance}[] = [];
+    console.log("Twilio Number: "+twilioNumber);
+    console.log("TwiML URL: "+url);
     for (let operator in operatorNumbers.split(";")) {
+        console.log("Operator: "+operator)
         const response = await client.calls.create(
             {
                 from: twilioNumber,
@@ -21,6 +24,6 @@ export async function createTwilioCalls(url: string): Promise<{operator: string,
         );
         results.push({operator, response});
     }
-    
+
     return results;
 }
