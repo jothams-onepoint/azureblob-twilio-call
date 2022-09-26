4326438d98296ce97fe295afe585c55b48c7bf30
JothamStickings <jotham.stickings@onepointltd.com>
2022-09-26 14:00:00 +0100

    better text to speech

--- fn-blob-to-twilio-call/twilioClient.ts
+++ fn-blob-to-twilio-call/twilioClient.ts
@@ -1,6 +1,7 @@
 import { Context } from "@azure/functions";
 import { Twilio } from "twilio";
 import { CallInstance } from "twilio/lib/rest/api/v2010/account/call";
+import VoiceResponse from "twilio/lib/twiml/VoiceResponse";
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
@@ -14,12 +15,20 @@ export async function createTwilioCalls(message: string, context: Context): Prom
     const results: {operator: string, response: CallInstance}[] = [];
     context.log("Twilio Number: "+twilioNumber);
     context.log("Message: "+message);
+
+    // construct twiML
+    const twiml = new VoiceResponse();
+    twiml.say({
+        voice: "man",
+        language:"en-GB"
+    }, message);
+
     const operatorNumbers = phoneList.split(";");
     for (let operator in operatorNumbers) {
         context.log("Calling Operator: "+operatorNumbers[operator]);
         const response = await client.calls.create(
             {
-                twiml: `<Response><Say>${message}</Say></Response>`,
+                twiml: twiml.toString(),
                 from: twilioNumber,
                 to: operatorNumbers[operator],
             }
