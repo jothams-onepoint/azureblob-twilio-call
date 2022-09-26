1795c8b94b1c3e7da0731b35c8aee4f2deefe95e
JothamStickings <jotham.stickings@onepointltd.com>
2022-09-26 10:41:42 +0100

    fix phone loop

--- fn-blob-to-twilio-call/twilioClient.ts
+++ fn-blob-to-twilio-call/twilioClient.ts
@@ -5,7 +5,7 @@ import { CallInstance } from "twilio/lib/rest/api/v2010/account/call";
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const twilioNumber = process.env.TWILIO_PHONE_NUMBER;
-const operatorNumbers = process.env.RECIPIENTS;
+const phoneList = process.env.RECIPIENTS;
 
 const client = new Twilio(accountSid, authToken);
 
@@ -14,12 +14,13 @@ export async function createTwilioCalls(url: string, context: Context): Promise<
     const results: {operator: string, response: CallInstance}[] = [];
     context.log("Twilio Number: "+twilioNumber);
     context.log("TwiML URL: "+url);
-    for (let operator in operatorNumbers.split(";")) {
-        context.log("Operator: "+operator)
+    const operatorNumbers = phoneList.split(";");
+    for (let operator in operatorNumbers) {
+        context.log("Calling Operator: "+operatorNumbers[operator]);
         const response = await client.calls.create(
             {
                 from: twilioNumber,
-                to: operator,
+                to: operatorNumbers[operator],
                 url: url
             }
         );
